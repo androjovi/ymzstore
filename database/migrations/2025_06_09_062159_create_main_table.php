9559fc099a3ba42d5cc25f6ce7ddb1deb5fdbd03
Joviandro <joviandro@wahana.com>
2025-06-09 14:02:14 +0700

    initialize commit

---
+++ database/migrations/2025_06_09_062159_create_main_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Schema::create('branch', function (Blueprint $table) {
+        //     $table->id()->primary();
+        //     $table->string('branch_code')->unique();
+        //     $table->string('branch_name', 265);
+        //     $table->text('branch_address')->nullable();
+        //     $table->bigInteger('created_by');
+        //     $table->timestamp('created_at')->nullable();
+        //     $table->timestamps();
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Schema::dropIfExists('main');
+    }
+};
